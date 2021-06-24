6a860d5bbb15ab8a13ea77c9967f78e37e5b89a4
kg4543 <jyc351919@naver.com>
2021-06-24 09:38:57 +0900

    데이터 연결 및 로그사용

---
+++ ERPAPP/ERPAPP/Helper/Common.cs
@@ -0,0 +1,19 @@
+﻿using ERPAPP.Model;
+using NLog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ERPAPP.Helper
+{
+    class Common
+    {
+        public static readonly Logger logger = LogManager.GetCurrentClassLogger();
+
+        public static tblUser LOGINED_USER;
+
+        public static tblItem SELECT_ITEM;
+    }
+}
---
+++ ERPAPP/ERPAPP/Logic/DataAcess.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ERPAPP.Model;
+
+namespace ERPAPP.Logic
+{
+    class DataAcess
+    {
+        public static List<tblUser> Getusers()
+        {
+            List<tblUser> users;
+
+            using(var ctx = new ERPEntities())
+            {
+                users = ctx.tblUser.ToList();
+            }
+
+            return users;
+        }
+        public static int SetUsers(tblUser user)
+        {
+            using (var ctx = new ERPEntities())
+            {
+                ctx.tblUser.AddOrUpdate(user);
+                return ctx.SaveChanges(); // commit
+            }
+        }
+
+        public static List<tblItem> GetItems()
+        {
+            List<tblItem> items;
+
+            using (var ctx = new ERPEntities())
+            {
+                items = ctx.tblItem.ToList();
+            }
+
+            return items;
+        }
+        public static int SetItems(tblItem item)
+        {
+            using (var ctx = new ERPEntities())
+            {
+                ctx.tblItem.AddOrUpdate(item);
+                return ctx.SaveChanges(); // commit
+            }
+        }
+
+        public static List<tblBrand> GetBrands()
+        {
+            List<tblBrand> brands;
+
+            using (var ctx = new ERPEntities())
+            {
+                brands = ctx.tblBrand.ToList();
+            }
+
+            return brands;
+        }
+        public static int SetBrands(tblBrand brand)
+        {
+            using (var ctx = new ERPEntities())
+            {
+                ctx.tblBrand.AddOrUpdate(brand);
+                return ctx.SaveChanges(); // commit
+            }
+        }
+
+        public static List<tblICate> GetICates()
+        {
+            List<tblICate> iCates;
+
+            using (var ctx = new ERPEntities())
+            {
+                iCates = ctx.tblICate.ToList();
+            }
+
+            return iCates;
+        }
+        public static int SetBrands(tblICate iCate)
+        {
+            using (var ctx = new ERPEntities())
+            {
+                ctx.tblICate.AddOrUpdate(iCate);
+                return ctx.SaveChanges(); // commit
+            }
+        }
+    }
+}
