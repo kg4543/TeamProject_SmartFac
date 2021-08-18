0ce4ef52d4c3f25b4f816fe47a2a950da2631091
sumin2123 <mini247269@naver.com>
2021-08-18 19:13:11 +0900

    commit

--- AGV/RobotArm/RobotArm_test_code.py
+++ AGV/RobotArm/RobotArm_test_code.py
@@ -1,5 +1,5 @@
-
 # -*- coding:utf-8 -*- #한글입력
+
 import RPi.GPIO as GPIO
 import time
 
@@ -18,54 +18,44 @@ up = GPIO.PWM(Upin, 50)
 
 hp.start(0)
 np.start(0)
-up.start(0)
+up.start(2.5)
+
 
 #각도별로 회전
 try:
-	while True:
+   while True:
 
-		up.ChangeDutyCycle(15)
-		print("내려가")
-		time.sleep(3)
+      hp.ChangeDutyCycle(2.5)
+      print("펼쳐")
+      time.sleep(3)
 
-		hp.ChangeDutyCycle(0)
-		print("펼쳐")
-		time.sleep(3)
+      up.ChangeDutyCycle(10)
+      print("내려가")
+      time.sleep(3)
 
-		hp.ChangeDutyCycle(10)
-		print("잡아")
-		time.sleep(3)
+      hp.ChangeDutyCycle(12)
+      print("잡아")
+      time.sleep(3)
 
-		np.ChangeDutyCycle(15)
-		print("머리내려")
-		time.sleep(3)
+      up.ChangeDutyCycle(2.5)
+      print("올려~!")
+      time.sleep(3)
 
-		up.ChangeDutyCycle(1)
-		print("올려~!")
-		time.sleep(3)
+      up.ChangeDutyCycle(10)
+      print("내려갓!")
+      time.sleep(3)
 
-		up.ChangeDutyCycle(20)
-		print("내려갓!")
-		time.sleep(3)
+      hp.ChangeDutyCycle(2.5)
+      print("놔")
+      time.sleep(3)
 
-		hp.ChangeDutyCycle(5)
-		print("놔")
-		time.sleep(3)
-
-		np.ChangeDutyCycle(0.1)
-		print("머리들어")
-		time.sleep(3)
-
-		up.ChangeDutyCycle(1)
-		print("다시올라와")
-		time.sleep(3)
-
-		break
+      up.ChangeDutyCycle(2.5)
+      print("다시올라와")
+      time.sleep(3)
+      
+      hp.ChangeDutyCycle(12)
+      print("close")
+      time.sleep(3)
 
 except KeyboardInterrupt:
-	GPIO.cleanup()
-
-
-# 지금 문제점이
-# 1. 로봇팔의 속도가 너무빠름 => for문으로 속도제어 필요
-# 2. head 모터부분에서 고장 발생 => 모터가 반대로 돌지 않음
+   GPIO.cleanup()
